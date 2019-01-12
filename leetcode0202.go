ab066126dcae85e4fb480d412d0df5f330de0e9f
Harry He <zhedahht@hotmail.com>
2019-01-12 00:20:01 -0800

    Add more solutions

---
+++ leetcode0202.go
@@ -0,0 +1,25 @@
+/*
+LeetCode 202: https://leetcode.com/problems/happy-number/
+*/
+
+package leetcode
+
+import "strconv"
+
+func isHappy(n int) bool {
+    visited := make(map[int]bool)
+    _, ok := visited[n]
+    for n != 1 && !ok {
+        visited[n] = true
+        
+        str := strconv.Itoa(n)
+        n = 0
+        for _, ch := range str {
+            n += int((ch - '0') * (ch - '0'))
+        }
+        
+        _, ok = visited[n]
+    }
+    
+    return n == 1
+}
