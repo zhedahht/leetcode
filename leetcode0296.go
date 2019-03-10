3a26dca8d0883fa1ce756c7b7363f0a5d9a933ea
Harry He <zhedahht@hotmail.com>
2019-03-10 10:14:57 -0700

    Create leetcode0296.go

---
+++ leetcode0296.go
@@ -0,0 +1,35 @@
+/*
+LeetCode 296: https://leetcode.com/problems/best-meeting-point/
+*/
+
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func minTotalDistance(grid [][]int) int {
+	rows, cols := make([]int, 0), make([]int, 0)
+	for i, _ := range grid {
+		for j, val := range grid[i] {
+			if val == 1 {
+				rows = append(rows, i)
+				cols = append(cols, j)
+			}
+		}
+	}
+
+	return minDist1D(rows) + minDist1D(cols)
+}
+
+func minDist1D(pos []int) int {
+	sort.Ints(pos)
+	mid := len(pos) / 2
+	sum := 0
+	for _, val := range pos {
+		sum += int(math.Abs(float64(pos[mid]) - float64(val)))
+	}
+
+	return sum
+}
