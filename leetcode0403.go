bf1937c78f7494072b3388a4fc79cc98ec4f72df
Harry He <harryh@microsoft.com>
2019-06-16 18:07:23 -0700

    Update solution for 403

--- leetcode0403.go
+++ leetcode0403.go
@@ -5,29 +5,38 @@ LeetCode 403: https://leetcode.com/problems/frog-jump/
 package leetcode
 
 func canCross(stones []int) bool {
-	dp := make([]map[int]bool, len(stones))
-	for i := range stones {
-		dp[i] = make(map[int]bool)
+	dp, stoneToIndex := make([][]bool, len(stones)), make(map[int]int)
+	for i, s := range stones {
+		dp[i] = make([]bool, len(stones))
+		stoneToIndex[s] = i
 	}
 
 	if stones[1] == 1 {
-		dp[1][1] = true
+		dp[0][1] = true
 	}
 
-	for i := 2; i < len(stones); i++ {
-		for j := i - 1; j >= 0; j-- {
-			if len(dp[j]) > 0 {
+	for i := 1; i < len(stones)-1; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j][i] {
 				dist := stones[i] - stones[j]
 				dists := [3]int{dist, dist - 1, dist + 1}
 				for _, d := range dists {
-					if _, exists := dp[j][d]; exists {
-						dp[i][dist] = true
-						break
+					if d > 0 {
+						pos := stones[i] + d
+						if next, exists := stoneToIndex[pos]; exists {
+							dp[i][next] = true
+						}
 					}
 				}
 			}
 		}
 	}
 
-	return len(dp[len(stones)-1]) > 0
+	for i := 0; i < len(stones); i++ {
+		if dp[i][len(stones)-1] {
+			return true
+		}
+	}
+
+	return false
 }
