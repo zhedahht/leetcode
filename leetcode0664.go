19e7c20dfe4a7dddde4737282006288b238eb8df
Harry He <harryh@microsoft.com>
2020-10-10 18:08:44 -0700

    Add solution for 664

---
+++ leetcode0664.go
@@ -0,0 +1,39 @@
+/*
+LeetCode 664: https://leetcode.com/problems/strange-printer/
+*/
+
+package leetcode
+
+func strangePrinter(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	dp := make([][]int, len(s))
+	for i := range dp {
+		dp[i] = make([]int, len(s))
+	}
+
+	for i := range s {
+		dp[i][i] = 1
+	}
+
+	for le := 2; le <= len(s); le++ {
+		for i := 0; i <= len(s)-le; i++ {
+			j := i + le - 1
+			dp[i][j] = le
+			for k := i + 1; k <= j; k++ {
+				turns := dp[i][k-1] + dp[k][j]
+				if s[k-1] == s[j] {
+					turns -= 1
+				}
+
+				if dp[i][j] > turns {
+					dp[i][j] = turns
+				}
+			}
+		}
+	}
+
+	return dp[0][len(s)-1]
+}
