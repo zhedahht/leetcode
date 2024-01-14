b02eae534af1e85f1a2c8b0e9c6c8cc152acdfcb
Harry He <hhe@rippling.com>
2024-01-13 17:57:28 -0800

    Add solution for 617

---
+++ leetcode0617.go
@@ -0,0 +1,29 @@
+/*
+LeetCode 617: https://leetcode.com/problems/merge-two-binary-trees/description/
+*/
+
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+
+	if root2 == nil {
+		return root1
+	}
+
+	return &TreeNode{
+		Val:   root1.Val + root2.Val,
+		Left:  mergeTrees(root1.Left, root2.Left),
+		Right: mergeTrees(root1.Right, root2.Right),
+	}
+}
